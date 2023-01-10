8fae8ca5f618d6063cdb59ef11ea7c5bd5c89e10
Sulamita Althaus <sulamitaalthaus44@gmail.com>
2023-01-10 14:04:46 -0300

    feat: connect with mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
+import { ConfigModule } from '@nestjs/config';
+
+ConfigModule.forRoot();
 
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.bptbrp4.mongodb.net/smartRanking?retryWrites=true&w=majority`,
+    ),
+    PlayersModule,
+  ],
   controllers: [],
   providers: [],
 })
