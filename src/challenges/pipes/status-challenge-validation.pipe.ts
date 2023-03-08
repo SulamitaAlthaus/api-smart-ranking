f937df3fe8f904aba1cc62766feb50058f6f230d
Sulamita Althaus <sulamitaalthaus44@gmail.com>
2023-03-08 12:53:33 -0300

    feat: create StatusChallengeValidationPipe

---
+++ src/challenges/pipes/status-challenge-validation.pipe.ts
@@ -0,0 +1,25 @@
+import { PipeTransform, BadRequestException } from '@nestjs/common';
+import { ChallengeStatus } from '../interfaces/challenge-status.enum';
+
+export class StatusChallengeValidationPipe implements PipeTransform {
+  readonly statusAllow = [
+    ChallengeStatus.ACCEPTED,
+    ChallengeStatus.DENIED,
+    ChallengeStatus.CANCELED,
+  ];
+  transform(value: any) {
+    const status = value.status.toUpperCase();
+    console.log('AQUI');
+
+    if (!this.statusValidation(status)) {
+      throw new BadRequestException(`${status} é um status inválido.`);
+    }
+
+    return value;
+  }
+
+  private statusValidation(status: any) {
+    const index = this.statusAllow.indexOf(status);
+    return index !== -1;
+  }
+}
